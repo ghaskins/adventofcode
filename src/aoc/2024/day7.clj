e95e7391a55ab35d00140922b4329ee122aabd0f
Greg Haskins <greg@manetu.com>
2024-12-29 11:11:47 -0500

    Apply @erdos optimization to Day 7
    See: https://github.com/erdos/advent-of-code/blob/master/2024/day07_numeric.clj
    
    Before:
    
    ```
    (time (day7/part2))
    "Elapsed time: 3391.66 msecs"
    ```
    
    After:
    
    ```
    (time (day7/part2))
    "Elapsed time: 6.220916 msecs"
    ```
    
    Signed-off-by: Greg Haskins <greg@manetu.com>

--- src/aoc/2024/day7.clj
+++ src/aoc/2024/day7.clj
@@ -1,6 +1,6 @@
 (ns aoc.2024.day7
   (:require [clojure.string :as string]
-            [clojure.math.combinatorics :as combo]))
+            [clojure.math :as math]))
 
 (defn parse []
   (->> (slurp "inputs/day7.txt")
@@ -10,28 +10,38 @@
                  (as-> $ (->> (apply cons $)
                               (map parse-long)))))))
 
-(defn compute [operands operators]
-  (->> (map vector operators (rest operands))
-       (reduce (fn [acc [operator operand]]
-                 (operator acc operand))
-               (first operands))))
+(defn cutoff [^long number ^long suffix]
+  (let [d (long (math/pow 10 (inc (int (math/log10 suffix)))))]
+    (when (= suffix (rem number d))
+      (quot number d))))
 
-(defn verify [operators [answer & operands]]
-  (let [operator-sets (combo/selections operators (dec (count operands)))]
-    (if (some #(= answer (compute operands %)) operator-sets)
-      answer
-      0)))
+; optimization inspired by https://github.com/erdos/advent-of-code/blob/master/2024/day07_numeric.clj
+(defn solve [operations ^long answer [^long n & n*]]
+  (if (seq n*)
+    (some (fn [f] (f operations answer n n*)) operations)
+    (= answer n)))
 
-(defn process [operators]
+(defn verify [operations [answer & operands]]
+  (if (solve operations answer (reverse operands))
+    answer
+    0))
+
+(defn process [operations]
   (->> (parse)
-       (pmap (partial verify operators))
+       (pmap (partial verify operations))
        (reduce + 0)))
 
-(defn part1 []
-  (process [+ *]))
+(defn addition [operations answer n n*]
+  (when (>= answer n) (solve operations (- answer n) n*)))
 
-(defn concatenation [a b]
-  (parse-long (str a b)))
+(defn multiplication [operations answer n n*]
+  (when (zero? (rem answer n)) (solve operations (quot answer n) n*)))
+
+(defn concatenation [operations answer n n*]
+  (some-> (cutoff answer n) long (as-> $ (solve operations $ n*))))
+
+(defn part1 []
+  (process [addition multiplication]))
 
 (defn part2 []
-  (process [+ * concatenation]))
+  (process [addition multiplication concatenation]))
